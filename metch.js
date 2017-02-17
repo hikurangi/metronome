32a35ea99bc2fd931fa2192baf72926606b15858
hikurangi <hikurangi@gmail.com>
2017-02-17 19:21:24 +1300

    bare bones scaffolding

---
+++ metch.js
@@ -0,0 +1,12 @@
+
+// set bpm
+let bpm = 120
+
+// convert that bpm into miliseconds
+let bpmInMs = 60000/bpm
+
+// metronome click function
+click = () => console.log("TICK!");
+
+// print the word "TICK!" at that rate
+setInterval(click, bpmInMs)
