ed2790d2883707f4ebc5deb7bce1ce18c1eeb65a
hikurangi <hikurangi@gmail.com>
2017-02-23 01:14:56 +1300

    config object?

--- metch.js
+++ metch.js
@@ -5,11 +5,16 @@ let bpm = 120 //needs sensible limitations, especially with subdivisions
 // convert that bpm into miliseconds
 let ms = 60000/bpm
 
-// bar length - default
+// bar length - default 4/4
 let barLength = 4
 
 // we start on (default to) beat 1 always
 let currentBeat = 1
+
+// subdivision - at this point it seems worth setting all these values in a config object and checking that every beat.
+let subdivision = {}
+
+
 
 // metronome click function
 click = () => {
