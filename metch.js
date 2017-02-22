eba235a7647d70254ca33199b4e5cd93bacfb8c0
hikurangi <hikurangi@gmail.com>
2017-02-22 23:52:03 +1300

    notes

--- metch.js
+++ metch.js
@@ -1,6 +1,6 @@
 
 // set bpm
-let bpm = 120
+let bpm = 120 //needs sensible limitations, especially with subdivisions
 
 // convert that bpm into miliseconds
 let ms = 60000/bpm
@@ -11,12 +11,14 @@ let currentBeat = 1
 
 // metronome click function
 click = () => {
+
   // log the current beat number
   console.log(`Beat #${currentBeat}`)
 
   // add one to it unless it is the same as the bar length
   currentBeat < barLength ? currentBeat++ : currentBeat = 1
+
 }
 
 // print the word "TICK!" at that rate
-setInterval(click, ms)
+setInterval(click, ms) // may want to be a setTimeout within a for loop.
