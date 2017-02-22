5ca0345c37e6ce26aa76b9ea30b1eeb1870f70bf
hikurangi <hikurangi@gmail.com>
2017-02-22 23:50:33 +1300

    functional beat counter

--- metch.js
+++ metch.js
@@ -3,10 +3,20 @@
 let bpm = 120
 
 // convert that bpm into miliseconds
-let bpmInMs = 60000/bpm
+let ms = 60000/bpm
+
+// bar length - default
+let barLength = 4
+let currentBeat = 1
 
 // metronome click function
-click = () => console.log("TICK!");
+click = () => {
+  // log the current beat number
+  console.log(`Beat #${currentBeat}`)
+
+  // add one to it unless it is the same as the bar length
+  currentBeat < barLength ? currentBeat++ : currentBeat = 1
+}
 
 // print the word "TICK!" at that rate
-setInterval(click, bpmInMs)
+setInterval(click, ms)
