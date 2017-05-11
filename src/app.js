90c2f10eacbedd9226773dce412a6ca7fd408320
hikurangi <hikurangi@gmail.com>
2017-05-11 13:39:53 +1200

    need to add higher pitched audio for beat one - will need to trim clicks to fit exactly together

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 "use strict"
 
+const clickSound = document.querySelector('.click')
+const beatOne = document.querySelector('.beat-one')
+
 let config = {
   bpm: 120, // set bpm
   get ms() {
     return 60000 / this.bpm
   }, // does this getter work with dynamically shifting bpm?
-  barLength: 9, // bar length - default 4/4
+  barLength: 4, // bar length in number of beats - default 4
   currentBeat: 1 // we start on (default to) beat 1 always
 }
 
@@ -14,6 +17,9 @@ let click = () => {
 
   // log the current beat number
   console.log(`Beat #${config.currentBeat}`)
+
+  // Play a click for every beat
+  clickSound.play()
 
   // add one to it unless it is the same as the bar length
   config.currentBeat < config.barLength ? config.currentBeat++ : config.currentBeat = 1
