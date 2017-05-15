c8f9a29db8dc2641888848817aceef9cd2a8127f
hikurangi <hikurangi@gmail.com>
2017-05-16 01:35:09 +1200

    still broken

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@
 
 let clickSound = document.querySelector('.click')
 
-let config = {
-  inputBPM: 400, // set bpm from input
+// config
+let state = {
+  inputBPM: 300, // set bpm from input
   tapTempoBPM: null, // set bpm from tap tempo
   get tapTempoMs() {
     return `no value yet ${this.tapTempoBPM}` // will use avg ms between taps. if setting tempo from this only start click from third tap?
@@ -19,31 +20,31 @@ let config = {
 
 clickSound.addEventListener("loadeddata", () => {
 
-  let loadLimit = Math.ceil(1000 * clickSound.duration / config.ms)
+  // determine the minimum number of cloned sounds required.
+  let loadLimit = Math.ceil(1000 * clickSound.duration / state.ms)
 
   let clickArr = []
 
+  // push cloned sounds to a storage array
   for (let i = 0; i < loadLimit; i++) {
+    // adding other subdivision/clicks will add a lot of complexity
     if ( clickArr.length < loadLimit ) {
       clickArr.push(clickSound.cloneNode())
-    } else {
-      break
     }
   }
 
-  console.log({clickArr, config});
+  console.log({clickArr, state});
   // metronome click function - simplified solution works for slow tempos only. need to trim sound
   let click = () => {
-    console.log(`Beat #${config.currentBeat}`)
+    console.log(`Beat #${state.currentBeat}`)
     // check if a sound is already playing or the sound has not been played (this second option will be replaced with a check for whether )
-    clickArr[config.arrPosition].play()
+    clickArr[state.arrPosition].play()
 
     // add one to it unless it is the same as the bar length
-    config.currentBeat < config.barLength ? config.currentBeat++ : config.currentBeat = 1
-
-    config.arrPosition < clickArr.length ? config.arrPosition++ : config.arrPosition = 0
+    state.currentBeat < state.barLength ? state.currentBeat++ : state.currentBeat = 1
+    state.arrPosition < clickArr.length ? state.arrPosition++ : state.arrPosition = 0
   }
 
-  if (config.clickOn) { setInterval(click, config.ms) }// may want to be a setTimeout within a for loop.
+  if (state.clickOn) { setInterval(click, state.ms) }// may want to be a setTimeout within a for loop.
 
 })
