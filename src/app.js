b7f00092ddea8a23bf7fcfe94bc10f3649a586aa
hikurangi <hikurangi@gmail.com>
2017-05-16 20:44:45 +1200

    fixed the bug - I got caught out by zero-based indexing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 "use strict"
 
-let clickSound = document.querySelector('.click')
+const clickSound = document.querySelector('.click')
 
 // config
-let state = {
-  inputBPM: 300, // set bpm from input
+const state = {
+  inputBPM: 255, // set bpm from input
   tapTempoBPM: null, // set bpm from tap tempo
   get tapTempoMS() {
     return `no value yet ${this.tapTempoBPM}` // will use avg ms between taps. if setting tempo from this only start click from third tap?
@@ -15,39 +15,46 @@ let state = {
   barLength: 4, // bar length in number of beats - default 4
   currentBeat: 1, // we start on (default to) beat 1 always
   arrPosition: 0, // track our position in the array of overlap sounds
-  clickOn: true
+  clickOn: true,
+  clickArr: []
 }
 
 clickSound.addEventListener("loadeddata", () => {
 
-  // determine the minimum number of cloned sounds required.
-  let loadLimit = Math.ceil(1000 * clickSound.duration / state.ms)
+  let loadLimit = Math.ceil(1000 * clickSound.duration / state.ms) // determine the minimum number of cloned sounds required. should change every time the tempo is changed
 
-  let clickArr = []
-
-  // push cloned sounds to a storage array
-  for (let i = 0; i < loadLimit; i++) {
+  for (let i = 0; i < loadLimit; i++) { // loop which will push cloned sounds to a storage array
     // adding other subdivision/clicks will add a lot of complexity
-    if ( clickArr.length > loadLimit ) {
-      clickArr = []
-      clickArr.push(clickSound)
-    } else if ( clickArr.length <= loadLimit ) {
-      clickArr.push(clickSound.cloneNode())
+    if ( state.clickArr.length > loadLimit ) { // if the array of stored clicks is bigger than the necessary click limit
+      state.clickArr = [] // reset the array
+      state.clickArr.push(clickSound) // push the original click sound to it
+    } else if ( state.clickArr.length < loadLimit ) { // otherwise if the clickArray is shorter than the click limit
+      state.clickArr.push(clickSound.cloneNode()) // add another clone of the clickSound to it
     }
+
   }
 
-  console.log({clickArr, state});
-  // metronome click function - simplified solution works for slow tempos only. need to trim sound
+  console.log('state.clickArr', state.clickArr, {state});
+
+  // metronome click function
+
   let click = () => {
-    console.log(`Beat #${state.currentBeat}`)
-    // check if a sound is already playing or the sound has not been played (this second option will be replaced with a check for whether )
-    clickArr[state.arrPosition].play()
+    // log which beat we're at. this should be delayed by half of the clickSound.duration so that it pops right about where the click itself is.
+    setTimeout(() => {console.log(`Beat #${state.currentBeat}`)}, clickSound.duration * 500) // * 1000 miliseconds / 2 halfway thru
 
-    // add one to it unless it is the same as the bar length
+    // play the click at the current position in the clickArr(ay)
+    console.log('state.clickArr', state.clickArr, {state});
+    state.clickArr[state.arrPosition].play()
+
+    // add one to the currentBeat tracker while it's less than the bar length, otherwise reset it to beat 1
     state.currentBeat < state.barLength ? state.currentBeat++ : state.currentBeat = 1
-    state.arrPosition < clickArr.length ? state.arrPosition++ : state.arrPosition = 0
+    // increment through the clickArray using the state's position tracker
+    state.arrPosition < state.clickArr.length - 1 ? state.arrPosition++ : state.arrPosition = 0
   }
 
-  if (state.clickOn) { setInterval(click, state.ms) }// may want to be a setTimeout within a for loop.
+  // if the click is set to on in the state, start the click for an interval set by state.ms.
+  if (state.clickOn) { setInterval(click, state.ms) } // may want to be a setTimeout within a for loop.
 
 })
+
+// add tap tempo
