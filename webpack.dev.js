0b37d298ea57beb88e7c32f138d9c4f23b5d7b13
hikurangi <hikurangi@gmail.com>
2017-05-19 23:23:31 +1200

    add extracttextplugin to the mix

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,22 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+import 'css/styles.css'
 
 module.exports = {
   entry: './app/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        use: 'css-loader'
+      })
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ]
 }
