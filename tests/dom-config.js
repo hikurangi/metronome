2c4f775275e920acbb1ccb3757416577311d2348
hikurangi <hikurangi@gmail.com>
2017-05-23 23:04:22 +1200

    now using yarn

---
+++ tests/dom-config.js
@@ -0,0 +1,13 @@
+// tests the dom-setup.js file
+
+import test from 'ava';
+
+test('foo', t => {
+	t.pass();
+});
+
+test('bar', async t => {
+	const bar = Promise.resolve('bar');
+
+	t.is(await bar, 'bar');
+});
