6c1cf84e513d6b906483b0d25c70822b6b7a0f4a
hikurangi <hikurangi@gmail.com>
2017-05-31 14:17:48 +1200

    comments

--- scripts/build-dev.js
+++ scripts/build-dev.js
@@ -3,20 +3,20 @@ const path = require('path')
 
 const buildDevDir = 'build-dev'
 
-// 3/2.5 change %PUBLIC_URL% in links to appropriate url in moved index.html
-const pathFix = () => {
-  console.log('this is where index.html gets pointed to the correct url');
-}
-
 // 1. Create the build-dev directory. Delete and create if it exists
 const buildSequence = new Promise ((resolve, reject) => {
   !fs.existsSync(buildDevDir) ? fs.mkdirSync(buildDevDir) : ( fs.removeSync(buildDevDir), fs.mkdirSync(buildDevDir) )
-})
+}) // must happen before files are copied to it.
   .then(fs.copy('./public', './build-dev')) // 2. copy index.html, favicon.ico, manifest.json from ../public/ to build-dev/
-  .then(pathFix())
+  .then(pathFix()) // 3. see at function definition
   .then(() => {
     console.log('success!');
   })
   .catch(err => {
     console.log({err});
   })
+
+// 3/2.5 change %PUBLIC_URL% in links to appropriate url in moved index.html
+const pathFix = () => {
+  console.log('this is where index.html gets pointed to the correct url');
+}
