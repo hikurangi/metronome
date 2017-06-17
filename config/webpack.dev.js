73e6855c3f80f2b9f1fbc3206e4adbc8621c510a
hikurangi <hikurangi@gmail.com>
2017-06-17 22:25:16 +1200

    simplified

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,28 +1,25 @@
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
+module.exports = config
+
+const config = {
   entry: './app/index.js',
   output: {
-    path: path.resolve(__dirname, '../build-dev'),
+    path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js'
   },
-  module: {
-    rules: [
-      { test: /\.css$/, use: ExtractTextPlugin.extract({ use: 'css-loader'}) },
-      { test: /\.wav$/, use: 'file-loader' }, // must go to static/audio
-      { test: /\.html$/, use: 'html-loader' } // base build folder
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, '../build-dev'),
-    compress: true,
-    port: 9000
-  },
-  plugins: [
-    new ExtractTextPlugin('styles.css')
-  ],
-  node: {
-    fs: 'empty'
-  }
+  // module: {
+  //   rules: [
+  //     { test: /\.css$/, use: ExtractTextPlugin.extract({ use: 'css-loader'}) },
+  //     { test: /\.wav$/, use: 'file-loader' }, // must go to static/audio
+  //     { test: /\.html$/, use: 'html-loader' } // base build folder
+  //   ]
+  // },
+  // plugins: [
+  //   new ExtractTextPlugin('styles.css')
+  // ],
+  // node: {
+  //   fs: 'empty'
+  // }
 }
